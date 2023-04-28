5330fe522a5d00c588c368b3769fc213fbf073a4
Dylan Sleith <sleithdylan@gmail.com>
2023-04-28 14:15:04 +0100

    feat: add data models

---
+++ Enums/MovieRating.cs
@@ -0,0 +1,12 @@
+﻿namespace Flixie.Enums
+{
+    public enum MovieRating
+    {
+        RATED_G,
+        RATED_PG,
+        RATED_12,
+        RATED_15,
+        RATED_16,
+        RATED_18
+    }
+}
