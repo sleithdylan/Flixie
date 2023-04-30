4d43720eabf7caeb3fc90b484dcb1594ad6c4331
Dylan Sleith <sleithdylan@gmail.com>
2023-04-30 14:35:37 +0100

    feat: add ActorsController

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,26 @@
+﻿using Flixie.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Flixie.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _mappingService;
+
+        public ActorsController(IRemoteMovieService tmdbMovieService, IDataMappingService mappingService)
+        {
+            _tmdbMovieService = tmdbMovieService;
+            _mappingService = mappingService;
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var actor = await _tmdbMovieService.ActorDetailAsync(id);
+            actor = _mappingService.MapActorDetail(actor);
+
+            return View(actor);
+        }
+    }
+}
